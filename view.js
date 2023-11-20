8fd802dd3f84af992fdd673c51b4b35b62819f7a
Eric Wei <eric.wei@wustl.edu>
2023-11-19 21:14:25 -0600

    add important notes on sidepanel

--- view.js
+++ view.js
@@ -1,79 +1,125 @@
 // ---- Define your dialogs  and panels here ----
-let new_effective_permission_panel = define_new_effective_permissions("effective_permission_panel", true, null);
-let user_select_field = define_new_user_select_field("effective_permission_panel", "user selection", function(selected_user){
-    $('#effective_permission_panel').attr('username', selected_user)
-    $('#effective_permission_panel').attr('filepath', '/C/presentation_documents/important_file.txt')
-});
-let permission_dialog = define_new_dialog("perm_dialog", title='Permissions', options = {});
-
+let new_effective_permission_panel = define_new_effective_permissions(
+  "effective_permission_panel",
+  true,
+  null
+);
+let user_select_field = define_new_user_select_field(
+  "effective_permission_panel",
+  "user selection",
+  function (selected_user) {
+    $("#effective_permission_panel").attr("username", selected_user);
+    $("#effective_permission_panel").attr(
+      "filepath",
+      "/C/presentation_documents/important_file.txt"
+    );
+  }
+);
+let permission_dialog = define_new_dialog(
+  "perm_dialog",
+  (title = "Permissions"),
+  (options = {})
+);
 
 // ---- Display file structure ----
 
 // (recursively) makes and returns an html element (wrapped in a jquery object) for a given file object
 function make_file_element(file_obj) {
-    let file_hash = get_full_path(file_obj)
+  let file_hash = get_full_path(file_obj);
 
-    if(file_obj.is_folder) {
-        let folder_elem = $(`<div class='folder' id="${file_hash}_div">
+  if (file_obj.is_folder) {
+    let folder_elem = $(`<div class='folder' id="${file_hash}_div">
             <h3 id="${file_hash}_header">
                 <span class="oi oi-folder" id="${file_hash}_icon"/> ${file_obj.filename} 
                 <button class="ui-button ui-widget ui-corner-all permbutton" path="${file_hash}" id="${file_hash}_permbutton"> 
                     <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"/> Edit Permissions
                 </button>
             </h3>
-        </div>`)
+        </div>`);
 
-        // append children, if any:
-        if( file_hash in parent_to_children) {
-            let container_elem = $("<div class='folder_contents'></div>")
-            folder_elem.append(container_elem)
-            for(child_file of parent_to_children[file_hash]) {
-                let child_elem = make_file_element(child_file)
-                container_elem.append(child_elem)
-            }
-        }
-        return folder_elem
+    // append children, if any:
+    if (file_hash in parent_to_children) {
+      let container_elem = $("<div class='folder_contents'></div>");
+      folder_elem.append(container_elem);
+      for (child_file of parent_to_children[file_hash]) {
+        let child_elem = make_file_element(child_file);
+        container_elem.append(child_elem);
+      }
     }
-    else {
-        return $(`<div class='file'  id="${file_hash}_div">
+    return folder_elem;
+  } else {
+    return $(`<div class='file'  id="${file_hash}_div">
             <span class="oi oi-file" id="${file_hash}_icon"/> ${file_obj.filename}
             <button class="ui-button ui-widget ui-corner-all permbutton" path="${file_hash}" id="${file_hash}_permbutton"> 
                 <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"/> Edit Permissions
             </button>
-        </div>`)
-    }
+        </div>`);
+  }
 }
 
-for(let root_file of root_files) {
-    let file_elem = make_file_element(root_file)
-    $( "#filestructure" ).append( file_elem);    
+for (let root_file of root_files) {
+  let file_elem = make_file_element(root_file);
+  $("#filestructure").append(file_elem);
 }
-
-
 
 // make folder hierarchy into an accordion structure
-$('.folder').accordion({
-    collapsible: true,
-    heightStyle: 'content'
-}) // TODO: start collapsed and check whether read permission exists before expanding?
-
+$(".folder").accordion({
+  collapsible: true,
+  heightStyle: "content",
+}); // TODO: start collapsed and check whether read permission exists before expanding?
 
 // -- Connect File Structure lock buttons to the permission dialog --
 
 // open permissions dialog when a permission button is clicked
-$('.permbutton').click( function( e ) {
-    // Set the path and open dialog:
-    let path = e.currentTarget.getAttribute('path');
-    perm_dialog.attr('filepath', path)
-    perm_dialog.dialog('open')
-    //open_permissions_dialog(path)
+$(".permbutton").click(function (e) {
+  // Set the path and open dialog:
+  let path = e.currentTarget.getAttribute("path");
+  perm_dialog.attr("filepath", path);
+  perm_dialog.dialog("open");
+  //open_permissions_dialog(path)
 
-    // Deal with the fact that folders try to collapse/expand when you click on their permissions button:
-    e.stopPropagation() // don't propagate button click to element underneath it (e.g. folder accordion)
-    // Emit a click for logging purposes:
-    emitter.dispatchEvent(new CustomEvent('userEvent', { detail: new ClickEntry(ActionEnum.CLICK, (e.clientX + window.pageXOffset), (e.clientY + window.pageYOffset), e.target.id,new Date().getTime()) }))
+  // Deal with the fact that folders try to collapse/expand when you click on their permissions button:
+  e.stopPropagation(); // don't propagate button click to element underneath it (e.g. folder accordion)
+  // Emit a click for logging purposes:
+  emitter.dispatchEvent(
+    new CustomEvent("userEvent", {
+      detail: new ClickEntry(
+        ActionEnum.CLICK,
+        e.clientX + window.pageXOffset,
+        e.clientY + window.pageYOffset,
+        e.target.id,
+        new Date().getTime()
+      ),
+    })
+  );
 });
 
+// make the div with the important notes about the interface:
+important_note_div = $(
+  '<div id="importent-note"><h1>Important Notes</h1><div>'
+);
+
+// array for all the notes
+var notes = [
+  '<strong>Direct</strong> permissions take precedence over <strong>inherited</strong> permissions (if any), and inherited permissions at a "closer" level to the file in question take precedence over ones that are "further" (e.g. parent permissions take precedence over grandparent permissions)',
+  'Within the same inheritance level, <strong>"deny"</strong> permissions take precedence over <strong>"allow"</strong> permissions.',
+  "Within the same inheritance level, permissions set for <strong>users</strong> and <strong>groups</strong> have the same priority; neither takes priority over the other.",
+];
+
+// iterate over each line of text and append it to the important-note div
+notes.forEach(function (line) {
+  important_note_div.append(
+    '<p style="margin-top: 10px; font-size: larger;">' + line + "</p>"
+  );
+});
+
+// set sidepanel css
+$("#sidepanel").css({
+  width: "45%",
+});
+
+// append div to sidepanel div
+$("#sidepanel").append(important_note_div);
 
 // ---- Assign unique ids to everything that doesn't have an ID ----
-$('#html-loc').find('*').uniqueId() 
+$("#html-loc").find("*").uniqueId();
